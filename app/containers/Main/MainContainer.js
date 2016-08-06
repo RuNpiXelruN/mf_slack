e1ff6f4ff40d91fc55f2e8d7933fc543fb358340
Justin Davidson <jutstindavidson23@gmail.com>
2016-08-06 20:59:57 +1000

    Webpack config complete, react app working, about to connect to redux

---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContainer = React.createClass({
+  render () {
+    return (
+      <h1>Heyyy from Boilerplate!!</h1>
+    )
+  }
+})
+
+export default MainContainer
